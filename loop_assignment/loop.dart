2348df6ee2250c7f3e81d2e85f48550c1c1f49a3
Muhammad ALI <99731097+muhammadali252002@users.noreply.github.com>
2025-01-02 23:24:17 +0500

    Update loop.dart

--- loop_assignment/loop.dart
+++ loop_assignment/loop.dart
@@ -86,3 +86,152 @@ void main() {
   }
   print("Number of vowels: $vowelCount");
 }*/
+/*Q6:Implement a code that finds the maximum and minimum elements in a
+list using a for loop and if-else condition.
+void main() {
+  List numbers = [12, 25, 36, 8, 65, 7, 98];
+
+  int max = numbers[0];
+  int min = numbers[0];
+
+  for (int i = 1; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    } else if (numbers[i] < min) {
+      min = numbers[i];
+    }
+  }
+
+  print("maximum value : $max");
+  print("minimum  value : $min");
+}*/
+
+/*Q7:Implement a code that finds the average of all the negative numbers in
+a list using a for loop and if-else condition.
+void main() {
+  List<int> numbers = [12, -4, -56, 23, -78, 1, -99, 3];
+
+  int sum = 0;
+  int count = 0;
+
+  for (int i = 0; i < numbers.length; i++) {
+    if (numbers[i] < 0) {
+      sum += numbers[i];
+      count++;
+    }
+  }
+  if (count > 0) {
+    double average = sum / count;
+    print("Average of negative number: $average");
+  } else {
+    print("No negative numbers find in list");
+  }
+}*/
+/*Q10:Write a program to display the cube of the number up to an integer.
+Test Data :
+Input number of terms : 5
+Expected Output :
+Number is : 1 and cube of the 1 is :1
+Number is : 2 and cube of the 2 is :8
+Number is : 3 and cube of the 3 is :27
+Number is : 4 and cube of the 4 is :64
+Number is : 5 and cube of the 5 is :125
+void main() {
+  int num = 8;
+
+  for (int i = 1; i <= num; i++) {
+    print("Number is : $i and the cube of the $i is : ${i * i * i}");
+  }
+}*/
+/*Q11:Write a program to display a pattern like a right angle triangle using an
+asterisk using loop.
+The pattern like :
+*
+**
+***
+****
+
+import 'dart:io';
+
+void main() {
+  for (int i = 1; i <= 5; i++) {
+    for (int j = 1; j <= i; j++) {
+      stdout.write("*");  // without a new line 
+    }
+    print(" ");
+  }
+}
+*/
+/*Q12:Write a program to display a pattern like a right angle triangle with a
+number using loop.
+The pattern like :
+1
+12
+123
+1234
+import 'dart:io';
+
+void main() {
+  for (int i = 1; i <= 4; i++) {
+    for (int j = 1; j <= i; j++) {
+      stdout.write(j);
+    }
+    print(" ");
+  }
+}*/
+/*Q13:Write a program to make such a pattern like a right angle triangle with
+a number which will repeat a number in a row.
+The pattern like :
+1
+22
+333
+4444
+import 'dart:io';
+
+void main() {
+  for (int i = 1; i <= 7; i++) {
+    for (var j = 1; j <= i; j++) {
+      stdout.write(i);
+    }
+    print(" ");
+  }
+}
+*/
+/*Q14:Write a program that asks the user for their email and password.You
+are given a list of predefined user credentials (email and password
+combinations). If the entered email and password match any of the
+credentials in the list, print "User login successful." Otherwise, keep
+asking for the email and password until the correct credentials are
+provided.
+
+import 'dart:io';
+
+void main() {
+  Map userCredentials = {
+    'user@example.com': 'pasword123',
+    'muhammadali252002@gmail.com': 'Ali25',
+    'test@dart.com': 'testpassword'
+  };
+
+  bool isLogin = false;
+
+  while (!isLogin) {
+    print("Enter your email");
+    String email = stdin.readLineSync()!;
+
+    print("Enter Your Password");
+    String password = stdin.readLineSync()!;
+
+    if (userCredentials.containsKey(email) &&
+        userCredentials[email] == password) {
+      print("User login successfully");
+      isLogin = true;
+    } else {
+      print("Invalid email or password.please Try again");
+    }
+  }
+}*/
+/*Q15:Write a program that takes a list of numbers as input and prints the
+numbers greater than 5 using a for loop and if-else condition.*/
+
+
